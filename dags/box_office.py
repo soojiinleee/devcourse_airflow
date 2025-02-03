d4402734c2d9a1c2742a93b77b3996328fdc0882
soojiinleee <soojiin.leee@gmail.com>
2025-02-03 18:37:31 +0900

    UPDATE : load task 수정

--- dags/box_office.py
+++ dags/box_office.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
 
 from airflow import DAG
+from airflow.decorators import task
 from airflow.operators.python import PythonOperator
 from airflow.models import Variable
 from airflow.providers.postgres.hooks.postgres import PostgresHook
@@ -18,6 +19,7 @@ def get_Redshift_connection(autocommit=True):
 
 def extract_transform(execution_date):
     logging.info("extract and transform started")
+    print("*"*10, execution_date)
 
     box_office_url = Variable.get("box_office_url")
     service_key = Variable.get("service_key")
@@ -32,37 +34,18 @@ def extract_transform(execution_date):
     result = []
 
     for boxof in root.findall('boxof'):
-        ranking = boxof.find('rnum').text if boxof.find('rnum') is not None else ""
-        performance_id = boxof.find('mt20id').text if boxof.find('mt20id') is not None else ""
-        performance_name = boxof.find('prfnm').text if boxof.find('prfnm') is not None else ""
-        genre = boxof.find('cate').text if boxof.find('cate') is not None else ""
-        performance_count = boxof.find('prfdtcnt').text if boxof.find('prfdtcnt') is not None else ""
-        area = boxof.find('area').text if boxof.find('area') is not None else ""
+        ranking = boxof.find('rnum').text if boxof.find('rnum') is not None else None
+        performance_id = boxof.find('mt20id').text if boxof.find('mt20id') is not None else None
+        performance_name = boxof.find('prfnm').text if boxof.find('prfnm') is not None else None
+        genre = boxof.find('cate').text if boxof.find('cate') is not None else None
+        performance_count = boxof.find('prfdtcnt').text if boxof.find('prfdtcnt') is not None else None
+        area = boxof.find('area').text if boxof.find('area') is not None else None
 
-        result.append([db_box_office_date, ranking, performance_id, performance_name, genre, performance_count, area])
+        result.append("('{}',{},'{}','{}','{}',{},'{}')".format(db_box_office_date, ranking, performance_id, performance_name, genre, performance_count, area))
 
-    # 결과 출력
     print(result)
     logging.info("extract and transform ended")
     return result
-
-def _create_table(cur, schema, table, drop_first):
-    if drop_first:
-        cur.execute(f"DROP TABLE IF EXISTS {schema}.{table};")
-    cur.execute(
-        f"""
-            CREATE TABLE IF NOT EXISTS {schema}.{table} (
-                box_office_date date,
-                ranking int,
-                performance_id varchar(150),
-                performance_name varchar(255),
-                genre varchar(100),
-                performance_count int,
-                area varchar(100),
-                created_at timestamp default GETDATE(),
-            );
-        """
-    )
 
 def load(**context):
     logging.info("load started")
@@ -72,34 +55,69 @@ def load(**context):
     records = context["task_instance"].xcom_pull(key="return_value", task_ids="extract_transform")
 
     cur = get_Redshift_connection()
-    try:
-        cur.execute("BEGIN;")
-        # 원본 테이블이 없으면 생성 - 테이블이 처음 한번 만들어질 때 필요한 코드
-        _create_table(cur, schema, table, False)
-        # 임시 테이블로 원본 테이블을 복사
-        cur.execute(f"CREATE TEMP TABLE t AS SELECT * FROM {schema}.{table};")
-        for r in records:
-            sql = f"INSERT INTO t VALUES ('{r[0]}', {r[1]}, '{r[2]}', '{r[3]}', '{r[4]}', {r[5]}, '{r[6]}');"
-            print(sql)
-            cur.execute(sql)
 
-        # 원본 테이블 생성
-        _create_table(cur, schema, table, True)
-        # 임시 테이블 내용을 원본 테이블로 복사
-        cur.execute(f"INSERT INTO {schema}.{table} SELECT DISTINCT * FROM t;")
+    # 원본 테이블이 없으면 생성
+    create_table_sql = f"""
+        CREATE TABLE IF NOT EXISTS {schema}.{table} (
+            box_office_date date,
+            ranking int,
+            performance_id varchar(150),
+            performance_name varchar(255),
+            genre varchar(100),
+            performance_count int,
+            area varchar(100),
+            created_at timestamp default GETDATE()
+            );
+    """
+    logging.info(create_table_sql)
+
+    # 임시 테이블 생성
+    create_t_sql = f"""CREATE TEMP TABLE t AS SELECT * FROM {schema}.{table};"""
+    logging.info(create_t_sql)
+    try:
+        cur.execute(create_table_sql)
+        cur.execute(create_t_sql)
         cur.execute("COMMIT;")
-    except Exception as error:
-        print(error)
+    except Exception as e:
         cur.execute("ROLLBACK;")
         raise
+
+    # 임시 테이블 데이터 입력
+    insert_sql = f"INSERT INTO t VALUES " + ",".join(records)
+    logging.info(insert_sql)
+    try:
+        cur.execute(insert_sql)
+        cur.execute("COMMIT;")
+    except Exception as e:
+        cur.execute("ROLLBACK;")
+        raise
+
+    # 기존 테이블 대체
+    alter_sql = f"""
+        DELETE FROM {schema}.{table};
+        INSERT INTO {schema}.{table}
+        SELECT box_office_date, ranking, performance_id, performance_name, genre, performance_count, area FROM (
+            SELECT *, ROW_NUMBER() OVER (PARTITION BY box_office_date, performance_id ORDER BY created_at DESC) seq
+            FROM t
+        )
+        WHERE seq = 1;
+    """
+    logging.info(alter_sql)
+    try:
+        cur.execute(alter_sql)
+        cur.execute("COMMIT;")
+    except Exception as e:
+        cur.execute("ROLLBACK;")
+        raise
+
     logging.info("load done")
 
 dag = DAG(
-    dag_id = 'Box_office_v1',
-    start_date = datetime(2025,2,1),
+    dag_id = 'Box_office_v2',
+    start_date = datetime(2025,1,1),
     catchup=True,
     tags=['API'],
-    schedule = '10 9 * * *' # 매일 12:10
+    schedule = '0 9 * * *' # 매일 12:10
 )
 
 
